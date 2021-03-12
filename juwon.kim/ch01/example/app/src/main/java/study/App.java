5612ec5d745252b342749a2a6332ff8dccb78532
appkr <juwonkim@me.com>
2021-03-13 08:43:49 +0900

    [example] 지저분해지는 코드 #1

--- juwon.kim/ch01/example/app/src/main/java/study/App.java
+++ juwon.kim/ch01/example/app/src/main/java/study/App.java
@@ -1,11 +1,11 @@
 package study;
 
 public class App {
-    public String getGreeting() {
-        return "Hello World!";
-    }
-
-    public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
-    }
+  public static void main(String[] args) {
+    Application app = new Application();
+    app.clicked(new Menu("menu1"));
+    app.clicked(new Button("button1"));
+    app.clicked(new Menu("menu2"));
+    app.clicked(new Button("button1"));
+  }
 }
--- juwon.kim/ch01/example/app/src/test/java/study/AppTest.java
+++ juwon.kim/ch01/example/app/src/test/java/study/AppTest.java
@@ -4,8 +4,8 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class AppTest {
-    @Test void appHasAGreeting() {
+    @Test void contextLoads() {
         App classUnderTest = new App();
-        assertNotNull(classUnderTest.getGreeting(), "app should have a greeting");
+        assertTrue(true);
     }
 }
