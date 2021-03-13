2f59ea42af8359aca1330f2de2827a72c05ceb0a
appkr <juwonkim@me.com>
2021-03-13 19:32:33 +0900

    [example] 절차지향 방식 코드 #1: 여성회원은 30일 유예기간

--- juwon.kim/ch02/example/app/src/main/java/study/MemberService.java
+++ juwon.kim/ch02/example/app/src/main/java/study/MemberService.java
@@ -5,8 +5,13 @@ import java.time.LocalDate;
 public class MemberService {
 
   public void handleRequest(Member member) {
-    if (member.getExpiryDate() != null &&
+    if (member.isMale() && member.getExpiryDate() != null &&
         member.getExpiryDate().isBefore(LocalDate.now())) {
+      System.out.println(String.format("%s님은 %s에 가입 기간이 만료되어 서비스를 사용하실 수 없습니다",
+          member.getName(),  member.getExpiryDate()));
+    }
+    if (!member.isMale() && member.getExpiryDate() != null &&
+        member.getExpiryDate().isBefore(LocalDate.now().minusDays(30))) {
       System.out.println(String.format("%s님은 %s에 가입 기간이 만료되어 서비스를 사용하실 수 없습니다",
           member.getName(),  member.getExpiryDate()));
     }
