9f15fe3fd9274e2cda921fe6f06fbf3519169bb6
appkr <juwonkim@me.com>
2021-03-13 21:48:20 +0900

    [example] paperboy: LoD 준수

--- juwon.kim/ch02/example/app/src/main/java/study/paperboy/Customer.java
+++ juwon.kim/ch02/example/app/src/main/java/study/paperboy/Customer.java
@@ -13,6 +13,19 @@ public class Customer {
   public Customer() {
   }
 
+  public int getPayment(int subscriptionPrice) {
+    if (wallet == null) {
+      throw new NotEnoughMoneyException(name + "님은 지금 지갑이 없습니다");
+    }
+    if (wallet.getTotalMoney() >= subscriptionPrice) {
+      wallet.subtractMoney(subscriptionPrice);
+      return subscriptionPrice;
+    }
+
+    throw new NotEnoughMoneyException(String.format("%s님의 지갑에 돈이 부족합니다: 신문값 %d, 지갑잔액 %d",
+        getName(), subscriptionPrice, wallet.getTotalMoney()));
+  }
+
   public String getName() {
     return name;
   }
