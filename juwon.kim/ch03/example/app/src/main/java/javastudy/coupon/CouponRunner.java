0c28fe9fe82c2f0b2509be5d2c02ba24a0f7e083
appkr <juwonkim@me.com>
2021-03-20 09:35:53 +0900

    [example] LimitPriceCoupon

--- juwon.kim/ch03/example/app/src/main/java/javastudy/coupon/CouponRunner.java
+++ juwon.kim/ch03/example/app/src/main/java/javastudy/coupon/CouponRunner.java
@@ -6,5 +6,9 @@ public class CouponRunner {
     final Coupon coupon = new Coupon(3000);
     final int price = coupon.calculateDiscountedPrice(5000);
     System.out.println("Discounted price: " + price);
+
+    final LimitedPriceCoupon lpCoupon = new LimitedPriceCoupon(5000, 1000);
+    System.out.println("When price is 10,000: " + lpCoupon.calculateDiscountedPrice(10000));
+    System.out.println("When price is 500: " + lpCoupon.calculateDiscountedPrice(500));
   }
 }
