4982aef1d57e0dee7290d6b428f4b1e995138524
Hyunseok Lee <smartbosslee@gmail.com>
2021-03-21 23:29:12 +0900

    Coupon 예제

--- hyunseok.lee/ch03/example/lhs-study-oop-ch03/src/App.java
+++ hyunseok.lee/ch03/example/lhs-study-oop-ch03/src/App.java
@@ -1,5 +1,9 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Product product = new Product(5000);
+        Coupon coupon = new Coupon(3000);
+        int price = coupon.calculateDiscountedPrice(product.getPrice());
+
+        System.out.println(price);
     }
 }
