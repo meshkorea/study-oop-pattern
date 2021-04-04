186d4a3589b539f801244bf409ffbd3b5e8cab89
YoungChulShin-Russel <youngchul.shin@meshkorea.net>
2021-04-04 20:06:24 +0900

    Context, State 구현

---
+++ youngchul.shin/ch07/state_example/src/main/java/pattern/state/states/ATMState.java
@@ -0,0 +1,9 @@
+package pattern.state.states;
+
+public interface ATMState {
+
+  void insertCard();
+  void ejectCard();
+  void insertPin(int pinEntered);
+  void requestCash(int cashToWithdraw);
+}
