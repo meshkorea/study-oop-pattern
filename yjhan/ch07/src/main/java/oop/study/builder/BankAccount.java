18bb17284442cf5c60629b022a06308f92a12971
Youngjun, Han <reverof81@gmail.com>
2021-04-26 04:37:55 +0900

    2nd. ver. wrong use case.

--- yjhan/ch07/src/main/java/oop/study/builder/BankAccount.java
+++ yjhan/ch07/src/main/java/oop/study/builder/BankAccount.java
@@ -4,12 +4,16 @@ public class BankAccount {
 
   private long accountNumber;
   private String owner;
+  private String branch;
   private double balance;
+  private double interestRate;
 
-  public BankAccount(long accountNumber, String owner, double balance) {
+  public BankAccount(long accountNumber, String owner, String branch, double balance, double interestRate) {
     this.accountNumber = accountNumber;
     this.owner = owner;
+    this.branch = branch;
     this.balance = balance;
+    this.interestRate = interestRate;
   }
 
   public long getAccountNumber() {
@@ -20,8 +24,16 @@ public class BankAccount {
     return owner;
   }
 
+  public String getBranch() {
+    return branch;
+  }
+
   public double getBalance() {
     return balance;
+  }
+
+  public double getInterestRate() {
+    return interestRate;
   }
 
 }
--- yjhan/ch07/src/test/java/oop/study/builder/BankAccountTest.java
+++ yjhan/ch07/src/test/java/oop/study/builder/BankAccountTest.java
@@ -10,11 +10,25 @@ class BankAccountTest {
   @Test
   @DisplayName("객체 생성")
   public void makeNewObjects() {
-    BankAccount bankAccount = new BankAccount(123L, "Bart", 100.00);
+    BankAccount bankAccount = new BankAccount(456L, "Marge", "Springfield", 100.00, 2.5);
 
-    assertEquals(123L, bankAccount.getAccountNumber());
-    assertEquals("Bart", bankAccount.getOwner());
+    assertEquals(456L, bankAccount.getAccountNumber());
+    assertEquals("Marge", bankAccount.getOwner());
+    assertEquals("Springfield", bankAccount.getBranch());
     assertEquals(100.00, bankAccount.getBalance());
+    assertEquals(2.5, bankAccount.getInterestRate());
+  }
+
+  @Test
+  @DisplayName("잘못된 사용의 예")
+  public void wrongUseExamples() {
+    BankAccount bankAccount = new BankAccount(789L, "Homer", null, 2.5, 100.00);
+
+    assertEquals(789L, bankAccount.getAccountNumber());
+    assertEquals("Homer", bankAccount.getOwner());
+    assertNull(bankAccount.getBranch());
+    assertEquals(100.00, bankAccount.getBalance());
+    assertEquals(2.5, bankAccount.getInterestRate());
   }
 
 }
